432141283e3f83a316bca4b448fa2ddbce38bcc1
JonatanHellgren <jonatan.hellgren@gmail.com>
2021-02-19 10:21:39 +0100

    simple for mundilfare

--- AI_DIT728/module5/code.py
+++ AI_DIT728/module5/code.py
@@ -6,11 +6,21 @@ import seaborn as sns
 with open('wdbc.pkl', 'rb') as f:
     wdbc = pickle.load(f)
 
-wdbc.head()
-features = wdbc.columns[2:]
-features[10]
-for feature in features:
-    sns.catplot(x='malignant', y=feature, kind='box', data=wdbc)
-    plt.title(feature)
-    plt.tight_layout()
-    plt.savefig(f'./plots/{feature}.png')
+
+def plot_data():
+    features = wdbc.columns[2:]
+    features[10]
+    for feature in features:
+        sns.catplot(x='malignant', y=feature, kind='box', data=wdbc)
+        plt.title(feature)
+        plt.tight_layout()
+        plt.savefig(f'./plots/{feature}.png')
+    return None
+
+
+def main():
+    plot_data()
+
+
+if __name__ == "__main__":
+    main()
